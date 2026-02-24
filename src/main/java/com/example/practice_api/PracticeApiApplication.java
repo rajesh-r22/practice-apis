d0b76fae62daf41f59487a2ec167c51df308b643
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-02-24 15:28:13 +0530

    first commit

---
+++ src/main/java/com/example/practice_api/PracticeApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.practice_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeApiApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
